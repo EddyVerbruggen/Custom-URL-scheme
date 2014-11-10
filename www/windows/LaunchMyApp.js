14f3ef0c96084f4ebff54d10444e40826a7ab4ec
Eion Robb <eion@robbmob.com>
2014-11-11 12:52:02 +1300

    Win8 version

---
+++ www/windows/LaunchMyApp.js
@@ -0,0 +1,10 @@
+
+
+var activatedHandler = function activatedHandler(e) {
+	if (typeof handleOpenURL == 'function' && e.uri) {
+		handleOpenURL(e.uri.rawUri);
+	}
+};
+
+var wui = Windows.UI.WebUI.WebUIApplication;
+wui.addEventListener("activated", activatedHandler, false);
