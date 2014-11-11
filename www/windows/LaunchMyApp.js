be33c7ecf0745f6b9e65c6282594f9aded49bf87
Eion Robb <eion@robbmob.com>
2014-11-12 09:11:57 +1300

    Fix namespacing of Windows JS

--- www/windows/LaunchMyApp.js
+++ www/windows/LaunchMyApp.js
@@ -1,10 +1,10 @@
-
-
-var activatedHandler = function activatedHandler(e) {
-	if (typeof handleOpenURL == 'function' && e.uri) {
-		handleOpenURL(e.uri.rawUri);
-	}
-};
-
-var wui = Windows.UI.WebUI.WebUIApplication;
-wui.addEventListener("activated", activatedHandler, false);
+(function () {
+	function activatedHandler(e) {
+		if (typeof handleOpenURL == 'function' && e.uri) {
+			handleOpenURL(e.uri.rawUri);
+		}
+	};
+	
+	var wui = Windows.UI.WebUI.WebUIApplication;
+	wui.addEventListener("activated", activatedHandler, false);
+}());
