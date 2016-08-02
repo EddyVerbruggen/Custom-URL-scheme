c0254b7fc274b975c4db7d07069515cdb3564fd9
Robert Schmid <r.schmid@outlook.com>
2016-08-01 17:14:18 -0700

    Use builtin Cordova event to retrieve URL on Windows

--- www/windows/LaunchMyApp.js
+++ www/windows/LaunchMyApp.js
@@ -1,10 +1,9 @@
 (function () {
-	function activatedHandler(e) {
-		if (typeof handleOpenURL == 'function' && e.uri) {
-			handleOpenURL(e.uri.rawUri);
-		}
-	};
-	
-	var wui = Windows.UI.WebUI.WebUIApplication;
-	wui.addEventListener("activated", activatedHandler, false);
+    function activatedHandler(e) {
+        if (typeof handleOpenURL === "function" && e.uri) {
+            handleOpenURL(e.uri.rawUri);
+        }
+    };
+
+    document.addEventListener("activated", activatedHandler, false);
 }());
