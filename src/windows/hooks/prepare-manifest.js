0b70d0c7c57e9a1fb6185a8fe7be37a64bcd71bd
David Watkins <david@davidofwatkins.com>
2019-11-12 23:21:30 -0800

    Replace incorrect usages of requireCordovaModule() with require()

--- src/windows/hooks/prepare-manifest.js
+++ src/windows/hooks/prepare-manifest.js
@@ -1,7 +1,7 @@
 module.exports = function(context) {
-    var fs = context.requireCordovaModule('fs'),
-        et = context.requireCordovaModule('elementtree'),
-        path = context.requireCordovaModule('path'),
+    var fs = require('fs'),
+        et = require('elementtree'),
+        path = require('path'),
         xml= context.requireCordovaModule('cordova-common').xmlHelpers,
         projectRoot = path.join(context.opts.projectRoot, "platforms", "windows");
 
